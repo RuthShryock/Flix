62ca1f9497d5ef9b79cf8583e14f4eb609dbcc31
RuthShryock <ruthieshryock@gmail.com>
2022-09-23 20:31:38 -0400

    adding part 2

---
+++ Flix-Part1/MoiveGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MoiveGrdiCell.swift
+//  Flix-Part1
+//
+//  Created by Ruth Shryock on 9/23/22.
+//
+
+import UIKit
+
+class MoiveGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
